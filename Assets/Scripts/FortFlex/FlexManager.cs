bdb5817e928f6ecb5a5a13fdb45ae81167d076df
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2021-12-17 14:09:32 -0500

    added material/shader, sending data to shader, refactored recursion

--- Assets/Scripts/FortFlex/FlexManager.cs
+++ Assets/Scripts/FortFlex/FlexManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TMPro;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
@@ -8,48 +9,72 @@ namespace Fort.Flex
 {
     public class FlexManager : MonoBehaviour
     {
+        public Material Material;
         private const float Z_INCREMENT_BY_LAYER = 0.05f;
         public Div Root;
 
         private void Start()
         {
-            Draw(Root, new ScreenBound(Screen.width, Screen.height), 0);
+            Root = new Div(
+                new float2(Screen.width/2f, Screen.height/2f), 
+                new float2(500, 40), 
+                new Color(0,1,0,1));
         }
 
-        public void Draw(Div parent, ScreenBound screenAbsolute, int layer)
+        private void Update()
         {
+            DrawRecurse(Root, new ScreenBound(Screen.width, Screen.height), 0);
+        }
+
+        public void DrawRecurse(Div parent, ScreenBound screenAbsolute, int layer)
+        {
+            var screenBoundsOfUI = Draw(parent, screenAbsolute, layer);
+            
             if (parent.Children == null)
                 return;
-            
+
             for (int i = 0; i < parent.Children.Count; i++)
             {
                 var child = parent.Children[i];
-                Mesh.MeshData data;
-                NativeArray<float3> meshVertices = new NativeArray<float3>(4, Allocator.Temp);
+                DrawRecurse(child, screenBoundsOfUI, layer + 1);
 
-                #region positioning
-                var childOriginWorld = child.LocalPosition + screenAbsolute.Value.xw;
-                float2 childEndWorld = childOriginWorld + child.WorldScale;
-                var childBoundWorld = new ScreenBound(childOriginWorld, childEndWorld);
+            }
+        }
 
+        public ScreenBound Draw(Div ui, ScreenBound drawBoundsScreen, int layer)
+        {
+
+            #region positioning screen
+
+                var posStartScreen = ui.LocalPosition + drawBoundsScreen.Value.xw;
+                float2 posEndScreen = posStartScreen + ui.WorldScale;
+                var boundsScreen = new ScreenBound(posStartScreen, posEndScreen);
                 #endregion
                 
-                #region to cpu side mesh
-                float z = layer * Z_INCREMENT_BY_LAYER;
-                float3 lb = new float3(childOriginWorld, z);
-                float3 rb = new float3(childEndWorld.x, childOriginWorld.y, z);
+                #region positioning normalized
+                float2 screenMax = new float2(Screen.width, Screen.height);
+                var posStartView = posStartScreen / screenMax;
+                var posEndView = posEndScreen / screenMax;
+                
+                #endregion
 
-                float3 lt = new float3(childOriginWorld.x, childEndWorld.y, z);
-                float3 rt = new float3(childEndWorld, z);
+                #region to cpu side mesh
+
+                float z = layer * Z_INCREMENT_BY_LAYER;
+                float3 lb = new float3(posStartView, z);
+                float3 rb = new float3(posEndView.x, posStartView.y, z);
+
+                float3 lt = new float3(posStartView.x, posEndView.y, z);
+                float3 rt = new float3(posEndView, z);
 
                 NativeArray<float3> vertices = new NativeArray<float3>(6, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
                 vertices[0] = lb;
                 vertices[1] = rb;
                 vertices[2] = lt;
                 vertices[3] = rt;
-                
+
                 NativeArray<int> indices = new NativeArray<int>(6, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
-                
+
                 // upper left tri
                 indices[0] = 0; //lb
                 indices[1] = 3; //rt
@@ -58,34 +83,39 @@ namespace Fort.Flex
                 indices[3] = 0; //lb
                 indices[4] = 1; //rb
                 indices[5] = 3; //rt
-                #endregion
-                
-                #region send cpu to side mesh to gpu
 
-                Mesh.MeshData data;
-                VertexAttributeDescriptor s;
-                s.attribute = VertexAttribute.Position;
-                s.dimension = 3;
-                s.format = VertexAttributeFormat.Float32;
-                Mesh mesh;
-                mesh.
-                data.SetVertexBufferParams(4, new NativeArray<VertexAttributeDescriptor>());
-                SubMeshDescriptor descriptor;
-                data.SetSubMesh();
                 #endregion
+
+                #region send data to gpu 
+                Mesh mesh = new Mesh();
+                mesh.SetVertices(vertices, 0, vertices.Length);
+                mesh.SetIndices(indices, 0, indices.Length);
+                
+                Material.SetVector("_BackgroundColor", ui.Color);
+                
+                Graphics.DrawMesh(mesh, float4x4.identity, Material, 0);
+
+                vertices.Dispose();
+                indices.Dispose();
+
+                // VertexAttributeDescriptor s;
+                // s.attribute = VertexAttribute.Position;
+                // s.dimension = 3;
+                // s.format = VertexAttributeFormat.Float32;
+                // data.SetVertexBufferParams(4, new NativeArray<VertexAttributeDescriptor>());
+                // SubMeshDescriptor descriptor;
+                // data.SetSubMesh();
+
+                #endregion
+
                 //use parent to and Position/scalle to get size.
                 //convert to normalzied coords
                 //convert to mesh
                 //draw...... mesh indirect
-                meshVertices.Dispose();
-                
-                
-                #region recurse
-                Draw(child, childBoundWorld, layer + 1);
-                #endregion
-            }
+
+                return boundsScreen;
         }
-        
+
 
         public struct ScreenBound
         {
@@ -97,15 +127,16 @@ namespace Fort.Flex
             {
                 Value = new float4(boundsX, boundsY);
             }
-            
-            public ScreenBound(float boundsX, float boundsY) 
+
+            public ScreenBound(float boundsX, float boundsY)
                 : this(new float2(0, boundsX), new float2(0, boundsY))
             {
             }
 
-            public static implicit operator float4 (ScreenBound bounds)
+            public static implicit operator float4(ScreenBound bounds)
             {
                 return bounds.Value;
+            }
         }
     }
 }
---
+++ Assets/Scripts/FortFlex/Shaders/DivShader.shader
@@ -0,0 +1,48 @@
+Shader "Unlit/DivShader"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            float4 _BackgroundColor;
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = _BackgroundColor;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
