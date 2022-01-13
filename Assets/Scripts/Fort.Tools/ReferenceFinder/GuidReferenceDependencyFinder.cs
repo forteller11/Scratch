8d5b97c571fbf9cc3d28fa1f60bbceed581d15a5
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2022-01-13 07:12:41 -0500

    Added a reference finder to find reference to unity assets (Guids)
    Is parallelized and async so it shouldn't block the editor and should utilize your cores

---
+++ Assets/Scripts/Fort.Tools/ReferenceFinder/GuidReferenceDependencyFinder.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.Jobs;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Fort.Tools.ReferenceFinder
+{
+    [Serializable]
+    public class GuidReferenceFinder
+    {
+        public string[] ExtensionsToCheck = new[] {
+            ".prefab", 
+            ".scene", 
+            ".mat", 
+            ".unity", //scene
+            ".vfx", //requires unity SRP
+            ".shadergraph", //requires unity SRP
+            ".preset"
+            
+        };
+
+        public List<FileInfo> ToProcess = new List<FileInfo>();
+
+        public List<string> DependencyPaths = new List<string>();
+        public List<Object> DependencyAssets = new List<Object>();
+
+        private int _filesProcessed = 0;
+
+        public float GetApproximateProgressNormalized()
+        {
+            if (ToProcess.Count == 0)
+                return 1;
+            
+            float progressNormalized = (float) _filesProcessed / ToProcess.Count;
+            return progressNormalized;
+        }
+        
+        public async Task FindDependencies(string toLookWithinAbsolutePath, string toFindGuid)
+        {
+            #region reset
+            _filesProcessed = 0;
+
+            ToProcess.Clear();
+            DependencyPaths.Clear();
+            DependencyAssets.Clear();
+            #endregion
+            
+            #region collect files to process
+            await Task.Factory.StartNew(() =>
+            {
+                DepthFirstRecurse(toLookWithinAbsolutePath);
+                Debug.Log(toLookWithinAbsolutePath);
+                Debug.Log($"Files To Process:\n Number:{ToProcess.Count}");
+
+            });
+            #endregion
+            
+            #region find refs from file contents
+            var depPathsSparse = new string [ToProcess.Count];
+
+            var findDepsTask = Task.Factory.StartNew(async () =>
+                {
+                    Task [] fileIOTasks = new Task[ToProcess.Count];
+                    for (int i = 0; i < ToProcess.Count; i++)
+                    {
+                        int closuredIndex = i;
+                        FileInfo fileInfo = ToProcess[i];
+                        var fileReadTask = File.ReadAllTextAsync(fileInfo.FullName); //make this a async stream / pooled thang' thing
+
+                        fileIOTasks[closuredIndex] = fileReadTask.ContinueWith(task =>
+                        {
+                            Interlocked.Increment(ref _filesProcessed);
+
+                            if (task.Exception != null)
+                            {
+                                Debug.LogError(task.Exception);
+                                return;
+                            }
+
+                            if (task.Result.Contains(toFindGuid)) //replace with something faster where you know what's happening
+                            {
+                                depPathsSparse[closuredIndex] = fileInfo.FullName;
+                            }
+                        });
+                    }
+                    await Task.WhenAll(fileIOTasks);
+                }
+            );
+            #endregion
+
+            // await Task.WhenAll(fileIOTasks);
+            await findDepsTask;
+            _filesProcessed = ToProcess.Count;
+            for (int i = 0; i < depPathsSparse.Length; i++)
+            {
+                if (depPathsSparse[i] != null)
+                {
+                    DependencyPaths.Add(depPathsSparse[i]);
+                }
+            }
+
+            ConvertAbsolutePathsToObjects();
+        }
+
+        private void ConvertAbsolutePathsToObjects()
+        {
+            for (int i = 0; i < DependencyPaths.Count; i++)
+            {
+                string depPathRelativeToAssets = GetRelativeToAssetsPath(DependencyPaths[i]);
+                var loadedObject = AssetDatabase.LoadAssetAtPath<Object>(depPathRelativeToAssets);
+                DependencyAssets.Add(loadedObject);
+            }
+        }
+
+        private void DepthFirstRecurse(string directoryPath)
+        {
+            //Save all children that should be guid reference searched
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+            foreach (var fileInfo in directory.EnumerateFiles())
+            {
+                //Add to check later if of correct file type
+                string fileExtension = fileInfo.Extension;
+                foreach (var extensionToProcess in ExtensionsToCheck)
+                {
+                    if (fileExtension == extensionToProcess)
+                    {
+                        ToProcess.Add(fileInfo);
+                    }
+                }
+            }
+
+            foreach (var childDirectories in Directory.EnumerateDirectories(directoryPath))
+            {
+                DepthFirstRecurse(childDirectories);
+            }
+        }
+
+        public static string GetAbsolutePath(string relativeToAssetsPath)
+        {
+            if (string.IsNullOrEmpty(relativeToAssetsPath))
+            {
+                Debug.LogWarning($"Could not get from {relativeToAssetsPath}, perhaps it is not an asset on disk?");
+                return null;
+            }
+
+            const string ASSETS_FOLDER_NAME = "Assets";
+            string assetsPathAbsolute = Application.dataPath;
+            string projectPathAbsolute = assetsPathAbsolute.Substring(0, assetsPathAbsolute.Length - ASSETS_FOLDER_NAME.Length); //removes "Assets" at end of datapath
+            
+            string absoluteRoot = projectPathAbsolute + relativeToAssetsPath;
+            return absoluteRoot;
+        }
+
+        public static string GetRelativeToAssetsPath(string absolutePath)
+        {
+            return GetRelativeToAssetsPath(absolutePath, Application.dataPath);
+        }
+        public static string GetRelativeToAssetsPath(string absolutePath, string projectPath)
+        {
+            string depPathRelativeToAssets = absolutePath.Remove(0, projectPath.Length - ("Assets".Length ));
+            depPathRelativeToAssets = depPathRelativeToAssets.Replace('\\', '/');
+            return depPathRelativeToAssets;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Fort.Tools/ReferenceFinder/GuidReferenceDependencyFinderEditor.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Fort.Tools.ReferenceFinder
+{
+    public class GuidReferenceFinderEditor : EditorWindow
+    {
+        [SerializeField] GuidReferenceFinder ReferenceFinder;
+        
+        public string FolderPathToLookInAbsolute = String.Empty;
+        
+        public string GuidToFind = String.Empty;
+        public Object AssetToFind = null;
+
+        public Vector2 ScrollAmount;
+        
+        [MenuItem("Test/Editor/GuidReferenceDependencyFinder")]
+        static void Init()
+        {
+            var target = GetWindow<GuidReferenceFinderEditor>();
+            target.Show();
+        }
+
+        private void OnEnable()
+        {
+            ReferenceFinder = new GuidReferenceFinder();
+        }
+
+        private void Update()
+        {
+            //repaint if progress is occuring
+            float progress = ReferenceFinder.GetApproximateProgressNormalized();
+            if (progress > 0 && progress < 1)
+            {
+                Repaint();
+            }
+
+        }
+
+        private void OnGUI()
+        {
+            DrawFolderPath(ref FolderPathToLookInAbsolute);
+            EditorGUILayout.Space();
+            DrawObjectGuidLinked("Asset To Find", ref AssetToFind, "Guid To Find", ref GuidToFind);
+            EditorGUILayout.Space();
+            
+            EditorGUILayout.Separator();
+            EditorGUI.BeginChangeCheck();
+            
+            if (GUILayout.Button("Find Dependencies"))
+            {
+                ReferenceFinder.FindDependencies(FolderPathToLookInAbsolute, GuidToFind).ContinueWith(task =>
+                {
+                    Repaint(); //so that progress and UI is updated appropriately on finish
+                }, TaskContinuationOptions.ExecuteSynchronously); //so the repaint happens on the MAIN thread and works
+            }
+            
+            float progress = ReferenceFinder.GetApproximateProgressNormalized();
+            if (progress > 0 && progress < 1)
+            {
+                DrawProgressBar(progress);
+            }
+
+            var dependencies = ReferenceFinder.DependencyPaths;
+
+            EditorGUILayout.Separator();
+            EditorGUILayout.LabelField($"Dependencies: {ReferenceFinder.DependencyPaths.Count}");
+
+            ScrollAmount = EditorGUILayout.BeginScrollView(ScrollAmount, false, false);
+            using (new EditorGUILayout.VerticalScope(GUI.skin.box))
+            {
+                EditorGUILayout.Space();
+                for (int i = 0; i < dependencies.Count; i++)
+                {
+                    string relativePath =
+                        GuidReferenceFinder.GetRelativeToAssetsPath(ReferenceFinder.DependencyPaths[i]);
+                    GUIContent label = new GUIContent(relativePath);
+                    label.tooltip = ReferenceFinder.DependencyPaths[i];
+                    label.text = relativePath;
+                    EditorGUILayout.ObjectField(label, ReferenceFinder.DependencyAssets[i], typeof(Object));
+                }
+            }
+            EditorGUILayout.EndScrollView();
+        }
+
+        public static void DrawObjectGuidLinked<T>(string assetLabel, ref T asset, string assetGuidLabel, ref string assetGuidText) where T : Object
+        {
+            using (new EditorGUILayout.VerticalScope(GUI.skin.box))
+            {
+                EditorGUI.BeginChangeCheck();
+                asset = (T) EditorGUILayout.ObjectField(assetLabel, asset, typeof(T), false);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    AssetDatabase.TryGetGUIDAndLocalFileIdentifier(asset, out assetGuidText, out long localID);
+                }
+
+                EditorGUI.BeginChangeCheck();
+                assetGuidText = EditorGUILayout.TextField(assetGuidLabel, assetGuidText);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    string assetPathFromGuidString = AssetDatabase.GUIDToAssetPath(assetGuidText);
+                    if (!string.IsNullOrEmpty(assetPathFromGuidString))
+                    {
+                        asset = AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(assetGuidText));
+                    }
+                    else
+                    {
+                        asset = null;
+                    }
+                }
+            }
+        }
+
+        public void DrawProgressBar(float progressNormalized)
+        {
+            EditorGUILayout.Slider(progressNormalized, 0, 1);
+        }
+        
+        public static void DrawFolderPath(ref string assetPathText)
+        {
+            EditorGUI.BeginChangeCheck();
+            using (new EditorGUILayout.VerticalScope(GUI.skin.box))
+            {
+                assetPathText = EditorGUILayout.TextField("To Search In", assetPathText);
+                
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button($"Choose Directory"))
+                    {
+                        assetPathText = EditorUtility.OpenFolderPanel("Choose Directory To Search Within", assetPathText, "folder");
+                    }
+                }
+            }
+        }
+    }
+}
